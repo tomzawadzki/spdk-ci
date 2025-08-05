28971a2eb643655a98d18da0f1403a0b0595bf33
Boris Glimcher <Boris.Glimcher@emc.com>
2025-08-06 02:55:48 +0300

    nvme: make zns more explicit configuration
    FreeBSD, for example doesn't support ZNS yet
    
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- cijoe/scripts/qemu_guest_start_custom_nvme.py
+++ cijoe/scripts/qemu_guest_start_custom_nvme.py
@@ -148,6 +148,104 @@ def qemu_nvme_args(nvme_img_root):
 
     # Nvme0n2 - ZNS namespace
     zoned_attributes = {
+        "zoned": "off",
+    }
+
+    drive2, qemu_nvme_dev2 = QemuNvme.generate_namespace(controller_id1, 2, lbads, nvme_img_root, zoned_attributes)
+    nvme += qemu_nvme_dev2
+    drives.append(drive2)
+
+    # Nvme1 - Controller dedicated to Fabrics testing
+    controller_id2 = "nvme1"
+    controller_bus2 = "pcie_downstream_port2"
+    controller_slot2 = 2
+    nvme += QemuNvme.generate_controller(
+        controller_id2, "adcdbeef", 7, controller_bus2, upstream_bus, controller_slot2
+    )
+
+    # Nvme1n1 - Namespace with NVM command-set
+    drive3, qemu_nvme_dev3 = QemuNvme.generate_namespace(controller_id2, 1, lbads, nvme_img_root)
+    nvme += qemu_nvme_dev3
+    drives.append(drive3)
+
+    # Nvme2 - Controller dedicated to testing HUGEPAGES / Large MDTS
+    controller_id3 = "nvme2"
+    controller_bus3 = "pcie_downstream_port3"
+    controller_slot3 = 3
+    nvme += QemuNvme.generate_controller(
+        controller_id3, "beefcace", 0, controller_bus3, upstream_bus, controller_slot3
+    )
+
+    # Nvme2n1 - NVM namespace
+    drive4, qemu_nvme_dev4 = QemuNvme.generate_namespace(controller_id3, 1, lbads, nvme_img_root)
+    nvme += qemu_nvme_dev4
+    drives.append(drive4)
+
+    #
+    # Nvme4 - Controller with PI enabled
+    #
+
+    controller_id5 = "nvme4"
+    controller_bus5 = "pcie_downstream_port5"
+    controller_slot5 = 5
+    nvme += QemuNvme.generate_controller(
+        controller_id5, "feebdaed", 7, controller_bus5, upstream_bus, controller_slot5
+    )
+
+    # Nvme4n1 - NVM namespace with PI type 1
+    drv_pi1, qemu_nvme_dev_pi1 = QemuNvme.generate_namespace(controller_id5, 1, lbads, nvme_img_root, {"ms": 8, "pi": 1})
+    nvme += qemu_nvme_dev_pi1
+    drives.append(drv_pi1)
+
+    # Nvme4n2 - NVM namespace with PI type 2
+    drv_pi2, qemu_nvme_dev_pi2 = QemuNvme.generate_namespace(controller_id5, 2, lbads, nvme_img_root, {"ms": 8, "pi": 2})
+    nvme += qemu_nvme_dev_pi2
+    drives.append(drv_pi2)
+
+    # Nvme4n3 - NVM namespace with PI type 3
+    drv_pi3, qemu_nvme_dev_pi3 = QemuNvme.generate_namespace(controller_id5, 3, lbads, nvme_img_root, {"ms": 8, "pi": 3})
+    nvme += qemu_nvme_dev_pi3
+    drives.append(drv_pi3)
+
+    return drives, nvme, drive_size
+
+def qemu_zns_nvme_args(nvme_img_root):
+    """
+    Returns list of drive-args, a string of qemu-arguments and drive size for each namespace
+
+    @returns drives, args, drive_size
+    """
+
+    lbads = 12
+    drive_size = "8G"
+
+    drives = []
+
+    # NVMe configuration arguments
+    nvme = []
+    nvme += ["-device", "pcie-root-port,id=pcie_root_port1,chassis=1,slot=1"]
+
+    upstream_bus = "pcie_upstream_port1"
+    nvme += ["-device", f"x3130-upstream,id={upstream_bus},bus=pcie_root_port1"]
+
+    #
+    # Nvme0 - Controller for functional verification of namespaces with NVM and ZNS
+    # command-sets
+    #
+    controller_id1 = "nvme0"
+    controller_bus1 = "pcie_downstream_port1"
+    controller_slot1 = 1
+    nvme += QemuNvme.generate_controller(
+        controller_id1, "deadbeef", 7, controller_bus1, upstream_bus, controller_slot1
+    )
+
+    # Nvme0n1 - NVM namespace
+    drive1, qemu_nvme_dev1 = QemuNvme.generate_namespace(controller_id1, 1, lbads, nvme_img_root)
+    nvme += qemu_nvme_dev1
+    drives.append(drive1)
+
+    # Nvme0n2 - ZNS namespace
+    zoned_attributes = {
         "zoned": "on",
         "zoned.zone_size": "32M",
         "zoned.zone_capacity": "28M",
@@ -283,6 +381,7 @@ def main(args, cijoe):
 
     nvme_setups = {
         "default": qemu_nvme_args,
+        "zns": qemu_zns_nvme_args,
         "ftl": qemu_ftl_nvme_args
     }
 
